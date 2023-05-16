b5714ab6ca76de3753586f87765e47ae097041fe
TestError0 <ashraf.inflack@gmail.com>
2023-05-16 10:16:15 +0600

    Java Frist Commit

---
+++ src/Index.java
@@ -0,0 +1,31 @@
+
+public class Index {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Hello Java");
+		
+		/*
+		int num1 = 5;
+		int num2 = 2;
+		int c = num1 + num2;
+		System.out.println("Sum of the calculation :" +c);
+		*/
+		
+		/*
+		float p = 2;
+		float q = 3;
+		float r = p + q;
+		System.out.println("Sub is : " +r);
+		*/
+		
+		/*
+		char a = 'C';
+		char b = 'D';
+		System.out.println("Char of B : " +b);
+		*/
+		
+	}
+
+}
---
+++ src/Main/Test.java
@@ -0,0 +1,12 @@
+package Main;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World");
+		System.out.println("Hello Java");
+
+	}
+
+}
