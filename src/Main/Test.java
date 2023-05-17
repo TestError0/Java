e6ffaf27a4d7f7bc6098e9bda1f98f44b2aa8158
TestError0 <ashraf.inflack@gmail.com>
2023-05-17 10:15:05 +0600

    Java sext day commit

--- src/Main/Test.java
+++ src/Main/Test.java
@@ -7,6 +7,7 @@ public class Test {
 		System.out.println("Hello World");
 		System.out.println("Hello Java");
 		System.out.println("Java Code running !!");
+		System.out.println("Next day COde !!");
 	}
 
 }
