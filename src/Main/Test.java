06dbc5698f5307ad31858070f33a3f1fcca841da
TestError0 <ashraf.inflack@gmail.com>
2023-05-23 16:35:31 +0600

    Java

--- src/Main/Test.java
+++ src/Main/Test.java
@@ -3,12 +3,9 @@ package Main;
 public class Test {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 		System.out.println("Hello World");
 		System.out.println("Hello Java");
-		System.out.println("Java Code running !!");
-		System.out.println("Next day COde !!");
-		System.out.println("Zero Work Today");
 	}
 
 }
